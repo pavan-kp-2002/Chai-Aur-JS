69e79e918e35af21b251f5f3523c99aba52ae876
Pavan K P <javalearner47@gmail.com>
2025-08-29 18:00:25 +0000

    Conversion types

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let age = ""
+
+const newAge = Number(age)
+//console.log(typeof newAge)
+//console.log(newAge)
+
+// "33" => 33
+// "33ab" => NaN (Not a number)
+// "" => 0
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 0 => false
+// 1 => true
+// undefined => false
+// " " => false
+// "1" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof someNumber)
